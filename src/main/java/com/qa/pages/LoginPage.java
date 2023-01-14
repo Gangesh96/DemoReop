4fe8b2a8f59034418a7b9a1faf08a90d0c75ea71
Your Name <you@example.com>
2023-01-14 16:13:45 +0530

    added search feacture code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,15 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void loginPage()
+	{
+		System.out.println("Login Page method");
+	}
+	public void loginSet()
+	{
+		System.out.println("Login set method");
+	}
+	
 
 }
